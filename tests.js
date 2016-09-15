3e3d17604e9c32baf6bf6ff3f725b38ead2443e2
Philipp Muens <raute1337@gmx.de>
2016-09-15 11:42:08 +0200

    Add custom authorizers test for API Gateway

--- tests.js
+++ tests.js
@@ -6,6 +6,7 @@ require('./aws/general/deploy-invoke-remove-lifecycle/tests');
 
 // API Gateway
 require('./aws/api-gateway/simple-api-setup/tests');
+require('./aws/api-gateway/custom-authorizers-setup/tests');
 
 // Custom resources
 require('./aws/general/custom-resources/tests');
