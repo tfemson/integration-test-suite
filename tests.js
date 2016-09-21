6cc46ae01d928d51666400c42cc5831115432264
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-09-21 20:31:10 +0700

    testing only schedule

--- tests.js
+++ tests.js
@@ -2,19 +2,19 @@
 
 // AWS
 // General
-require('./aws/general/deploy-invoke-remove-lifecycle/tests');
+// require('./aws/general/deploy-invoke-remove-lifecycle/tests');
 
 // API Gateway
-require('./aws/api-gateway/simple-api-setup/tests');
-require('./aws/api-gateway/custom-authorizers-setup/tests');
-require('./aws/api-gateway/cors-setup/tests');
+// require('./aws/api-gateway/simple-api-setup/tests');
+// require('./aws/api-gateway/custom-authorizers-setup/tests');
+// require('./aws/api-gateway/cors-setup/tests');
 
 // Schedule
 require('./aws/schedule/multipleSchedules-multipleFunctions/tests');
 
 // Custom resources
-require('./aws/general/custom-resources/tests');
+// require('./aws/general/custom-resources/tests');
 
 // General (Serverless)
 // Custom Plugin
-require('./general/serverless/custom-plugins/tests');
+// require('./general/serverless/custom-plugins/tests');
