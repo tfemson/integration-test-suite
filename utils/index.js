27262186c862552c86ad85fc72f643f872d4fd93
Philipp Muens <raute1337@gmx.de>
2016-09-27 11:06:15 +0200

    Refactor name of tmpDirs so that it's shorter
    And the created tmpDirs are grouped by repository.

--- utils/index.js
+++ utils/index.js
@@ -16,7 +16,8 @@ module.exports = {
   createTestService: (templateName, testServiceDir) => {
     const serviceName = `service-${(new Date()).getTime().toString()}`;
     const tmpDir = path.join(os.tmpdir(),
-      'tmpdirs-serverless-integration-test-suite',
+      'tmpdirs-serverless',
+      'integration-test-suite',
       crypto.randomBytes(8).toString('hex'));
 
     fse.mkdirsSync(tmpDir);
